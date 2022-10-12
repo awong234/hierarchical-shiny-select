657f66f161a75538fa3feb258f412f2344f7bdfc
Alec Wong <aw685@cornell.edu>
2022-10-12 00:14:47 -0400

    Add stack of events

--- app1.R
+++ app1.R
@@ -48,7 +48,8 @@ ui <- fluidPage(
                           ),
             shiny::column(3, shiny::p("Class D1 lacks E1."))
         ),
-        shiny::p("Data are structured this way so that most selectors must update something on each input.")
+        shiny::p("Data are structured this way so that most selectors must update something on each input."),
+        shiny::verbatimTextOutput('stack')
 
     )
 )
@@ -62,47 +63,62 @@ server <- function(input, output, session) {
         size = fs::dir_info(glob = "data.db")[['size']]
         paste0("Dataset has ", nrows, " rows (", size, " on disk)")
     })
+    output$stack = renderText({
+        paste0("Order of events\n", paste0("Update ", stack(), collapse = "\n"))
+    })
     # Reactive to obtain data at A level
     A_lvl = reactive({
-        log_debug("Filtering colA")
+        log_trace("Filtering colA")
         df |> filter(colA == !!input$colA)
     })
     # Set choices for B
+    stack = reactiveVal('start')
+    push = function(stack, add) {
+        c(stack(), add)
+    }
+    log_event = function(letter, stack) {
+        last_stack = tail(stack(), 1)
+        log_debug("Getting unique col{letter} to set selectInput choices: coming from {last_stack}")
+    }
     observeEvent(A_lvl(), {
-        log_debug("Getting unique colB")
+        log_event("B", stack)
+        stack(push(stack, "B"))
         choices = A_lvl() |> distinct(colB) |> collect() |> pull() |> sort()
         updateSelectInput(session, 'colB', choices = choices)
     })
     # Reactive to obtain data at A-B level
     B_lvl = reactive({
-        log_debug("Filtering colB")
+        log_trace("Filtering colB")
         A_lvl() |> filter(colB == !!input$colB)
     })
     # Set choices for C
     observeEvent(B_lvl(), {
-        log_debug("Getting unique colC")
+        log_event("C", stack)
+        stack(push(stack, "C"))
         choices = B_lvl() |> distinct(colC) |> collect() |> pull() |> sort()
         updateSelectInput(session, 'colC', choices = choices)
     })
     # Reactive to obtain data at A-B-C level
     C_lvl = reactive({
-        log_debug("Filtering colC")
+        log_trace("Filtering colC")
         B_lvl() |> filter(colC == !!input$colC)
     })
     # Set choices for D
     observeEvent(C_lvl(), {
-        log_debug("Getting unique colD")
+        log_event("D", stack)
+        stack(push(stack, "D"))
         choices = C_lvl() |> distinct(colD) |> collect() |> pull() |> sort()
         updateSelectInput(session, 'colD', choices = choices)
     })
     # Reactive to obtain data at A-B-C-D level
     D_lvl = reactive({
-        log_debug("Filtering colD")
+        log_trace("Filtering colD")
         C_lvl() |> filter(colD == !!input$colD)
     })
     # Set choices for E
     observeEvent(D_lvl(), {
-        log_debug("Getting unique colE")
+        log_event("E", stack)
+        stack(push(stack, "E"))
         choices = D_lvl() |> distinct(colE) |> collect() |> pull() |> sort()
         updateSelectInput(session, 'colE', choices = choices)
     })
