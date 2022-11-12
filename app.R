a37b3fcc12fc18dfa242c97eb9d5bcd73f9a3b54
Alec Wong <aw685@cornell.edu>
2022-11-11 22:35:56 -0500

    update reactive structure

--- app.R
+++ app.R
@@ -70,57 +70,91 @@ server <- function(input, output, session) {
     push = function(stack, add) {
         c(stack(), add)
     }
-    # Reactive to obtain data at A level
-    A_lvl = reactive({
-        log_trace("Filtering colA")
-        df |> filter(colA == !!input$colA)
-    })
-    # Set choices for B
     log_event = function(letter, stack) {
         last_stack = tail(stack(), 1)
         log_debug("Getting unique col{letter} to set selectInput choices: coming from {last_stack}")
     }
-    observeEvent(A_lvl(), {
-        log_event("B", stack)
-        stack(push(stack, "B"))
-        choices = A_lvl() |> distinct(colB) |> collect() |> pull() |> sort()
-        updateSelectInput(session, 'colB', choices = choices)
+    valid_sel = function(value, valid_values) {
+        value_valid = value %in% valid_values
+        if (isTRUE(value_valid)) {
+            selection = value
+        } else {
+            selection = valid_values[1]
+        }
+        return(selection)
+    }
+
+    # Reactive to obtain data at A level
+    A_lvl = reactive({
+        log_trace("Filtering colA")
+        df |> filter(colA == !!input$colA)
     })
     # Reactive to obtain data at A-B level
     B_lvl = reactive({
         log_trace("Filtering colB")
         A_lvl() |> filter(colB == !!input$colB)
     })
-    # Set choices for C
-    observeEvent(B_lvl(), {
-        log_event("C", stack)
-        stack(push(stack, "C"))
-        choices = B_lvl() |> distinct(colC) |> collect() |> pull() |> sort()
-        updateSelectInput(session, 'colC', choices = choices)
-    })
     # Reactive to obtain data at A-B-C level
     C_lvl = reactive({
         log_trace("Filtering colC")
         B_lvl() |> filter(colC == !!input$colC)
-    })
-    # Set choices for D
-    observeEvent(C_lvl(), {
-        log_event("D", stack)
-        stack(push(stack, "D"))
-        choices = C_lvl() |> distinct(colD) |> collect() |> pull() |> sort()
-        updateSelectInput(session, 'colD', choices = choices)
     })
     # Reactive to obtain data at A-B-C-D level
     D_lvl = reactive({
         log_trace("Filtering colD")
         C_lvl() |> filter(colD == !!input$colD)
     })
+
+    update_defaults = function() {
+        log_debug("UPDATING DEFAULTS")
+        updateSelectInput(session, 'colA', choices = paste0("A", 1:5), selected = "A1")
+        updateSelectInput(session, 'colB', choices = paste0("B", 1:5), selected = "B2")
+        updateSelectInput(session, 'colC', choices = paste0("C", 1:5), selected = "C3")
+        updateSelectInput(session, 'colD', choices = paste0("D", 1:5), selected = "D4")
+        updateSelectInput(session, 'colE', choices = paste0("E", 1:5), selected = "E5")
+    }
+
+    update_defaults()
+
+    # Set choices for B
+    observeEvent(A_lvl(), {
+        log_event("B", stack)
+        stack(push(stack, "B"))
+        validate(need(input$colB, label = "colB"))
+        value = input$colB
+        choices = A_lvl() |> distinct(colB) |> collect() |> pull() |> sort()
+        selection = valid_sel(value, choices)
+        updateSelectInput(session, 'colB', choices = choices, selected = selection)
+    })
+    # Set choices for C
+    observeEvent(B_lvl(), {
+        log_event("C", stack)
+        stack(push(stack, "C"))
+        validate(need(input$colC, label = "colB"))
+        value = input$colC
+        choices = B_lvl() |> distinct(colC) |> collect() |> pull() |> sort()
+        selection = valid_sel(value, choices)
+        updateSelectInput(session, 'colC', choices = choices, selected = selection)
+    })
+    # Set choices for D
+    observeEvent(C_lvl(), {
+        log_event("D", stack)
+        stack(push(stack, "D"))
+        validate(need(input$colB, label = "colB"))
+        value = input$colB
+        choices = C_lvl() |> distinct(colD) |> collect() |> pull() |> sort()
+        selection = valid_sel(value, choices)
+        updateSelectInput(session, 'colD', choices = choices, selected = selection)
+    })
     # Set choices for E
     observeEvent(D_lvl(), {
         log_event("E", stack)
         stack(push(stack, "E"))
+        validate(need(input$colB, label = "colB"))
+        value = input$colB
         choices = D_lvl() |> distinct(colE) |> collect() |> pull() |> sort()
-        updateSelectInput(session, 'colE', choices = choices)
+        selection = valid_sel(value, choices)
+        updateSelectInput(session, 'colE', choices = choices, selected = selection)
     })
 
 
